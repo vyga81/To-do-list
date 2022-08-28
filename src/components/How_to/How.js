4a3d35470d06b79809c95e9bf9d10da9537099e6
Vygantas Kazakevicius <104910000+vyga81@users.noreply.github.com>
2022-08-29 00:33:57 +0200

    changed background to how

--- src/components/How_to/How.js
+++ src/components/How_to/How.js
@@ -1,2 +1,15 @@
 
 
+import React from 'react';
+import image from '../../Images/nightcity2'
+
+import './Home.css';
+
+const How = () => (
+    <div className="Home" data-testid="Home">
+        <img src={image} className='How-nightcity2-img' alt='jpg' />
+    </div>
+);
+
+
+export default How;
